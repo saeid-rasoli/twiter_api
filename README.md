d99cdfa02c61825a3569588bf5e661130df5e7ab
saeid-rasoli <saeidrasoli1212@gmail.com>
2024-08-20 03:12:12 +0330

    update readme

--- README.md
+++ README.md
@@ -7,3 +7,21 @@ for fetching data we use tweepy and for using this method we use twitter (x) api
 
 **NOTE**
 all before twitter api features such as fetching tweets, get all tweets infos such as number of likes, retweets, shares and ..., has been gone on free edition, and all of these features will work only on paid edition.
+
+**Examples:**
+
+endpoint /
+```json
+{
+  "message": "welcome, please append username to the url to get user info, and don't forget to active proxy"
+}
+```
+
+endpoint /username
+```json
+{
+  "name": "Elon Musk",
+  "id": 44196397,
+  "username": "elonmusk"
+}
+```
--- main.py
+++ main.py
@@ -4,17 +4,24 @@ import json
 
 app = FastAPI()
 
-@app.get('/{username}')
-async def root(username: str):
+
+@app.get("/")
+async def root():
+    message = "welcome, please append username to the url to get user info, and don't forget to active proxy"
+    return {"message": message}
+
+
+@app.get("/{username}")
+async def get_usename_data(username: str):
     with open("twitter_keys.json") as infile:
         json_obj = json.load(infile)
         bearer_token = json_obj["bearer_token"]
-    
+
     api = tweepy.Client(bearer_token=bearer_token, wait_on_rate_limit=True)
-    print('Authenticated...\n')
+    print("Authenticated...\n")
 
     user = api.get_user(username=username)
-    get_name = user.data['name']
-    get_id = user.data['id']
-    get_username = user.data['username']
-    return {'name': get_name, 'id': get_id, 'username': get_username}
+    get_name = user.data["name"]
+    get_id = user.data["id"]
+    get_username = user.data["username"]
+    return {"name": get_name, "id": get_id, "username": get_username}
