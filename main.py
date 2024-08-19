750755a89e9b802808a8208b85c687746dfd4218
saeid-rasoli <saeidrasoli1212@gmail.com>
2024-08-20 01:30:56 +0330

    remove tweepy_main file and move it to the main.py file

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+import tweepy
+import json
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'message': 'hello'}
+@app.get('/{username}')
+async def root(username: str):
+    with open("twitter_keys.json") as infile:
+        json_obj = json.load(infile)
+        bearer_token = json_obj["bearer_token"]
+    
+    api = tweepy.Client(bearer_token=bearer_token, wait_on_rate_limit=True)
+    print('Authenticated...\n')
+
+    user = api.get_user(username=username)
+    get_name = user.data['name']
+    get_id = user.data['id']
+    get_username = user.data['username']
+    return {'name': get_name, 'id': get_id, 'username': get_username}
